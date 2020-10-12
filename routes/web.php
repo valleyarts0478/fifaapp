f0c1b5528537b60f294e3ddc90d8fe2a411d7585
valleyarts0478 <arts047855@yahoo.co.jp>
2020-10-12 21:42:45 +0900

    admin_madeOK

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+//管理側
+Route::group(['middleware' => ['auth.admin']], function () {
+	
+	//管理側トップ
+	Route::get('/admin', 'admin\AdminTopController@show');
+	//ログアウト実行
+	Route::post('/admin/logout', 'admin\AdminLogoutController@logout');
+	//ユーザー一覧
+	Route::get('/admin/user_list', 'admin\ManageUserController@showUserList');
+	//ユーザー詳細
+	Route::get('/admin/user/{id}', 'admin\ManageUserController@showUserDetail');
+
+});
+
+//管理側ログイン
+Route::get('/admin/login', 'admin\AdminLoginController@showLoginform');
+Route::post('/admin/login', 'admin\AdminLoginController@login');
